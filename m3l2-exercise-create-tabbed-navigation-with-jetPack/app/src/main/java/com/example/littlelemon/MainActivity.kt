e91cef070a56ad2bdb62a4219a1785ae854b465f
rafiyq <rafiyq@outlook.com>
2023-06-22 13:20:32 +0700

    Create the Bottom Navigation composable

--- m3l2-exercise-create-tabbed-navigation-with-jetPack/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ m3l2-exercise-create-tabbed-navigation-with-jetPack/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,12 +3,54 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+        }
+    }
+}
+
+@Composable
+fun MyBottomNavigation(navController: NavController) {
+    val destinationList = listOf(
+        Menu,
+        Home,
+        Location
+    )
+    val selectedIndex = rememberSaveable {
+        mutableStateOf(1)
+    }
+    BottomNavigation {
+        destinationList.forEachIndexed { index, destination ->
+            BottomNavigationItem(
+                label = { Text(text = destination.title) },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = destination.icon),
+                        contentDescription = destination.title
+                    )
+                },
+                selected = index == selectedIndex.value,
+                onClick = {
+                    selectedIndex.value = index
+                    navController.navigate(destinationList[index].route) {
+                        launchSingleTop = true
+                        popUpTo(Home.route)
+                    }
+                }
+            ) { }
         }
     }
 }

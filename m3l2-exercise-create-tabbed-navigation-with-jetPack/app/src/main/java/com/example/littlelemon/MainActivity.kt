d2b1afc0e5c2803a15045be4c7d17a443c5622e4
rafiyq <rafiyq@outlook.com>
2023-06-22 13:50:38 +0700

    Finalize the top-level composable

--- m3l2-exercise-create-tabbed-navigation-with-jetPack/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ m3l2-exercise-create-tabbed-navigation-with-jetPack/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,21 +3,42 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MyApp()
+        }
+    }
+}
 
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { MyBottomNavigation(navController = navController) }) {
+        Box(Modifier.padding(it)) {
+            NavHost(navController = navController, startDestination = Home.route) {
+                composable(Menu.route) { MenuScreen() }
+                composable(Home.route) { HomeScreen() }
+                composable(Location.route) { LocationScreen() }
+            }
         }
     }
 }
@@ -30,7 +51,7 @@ fun MyBottomNavigation(navController: NavController) {
         Location
     )
     val selectedIndex = rememberSaveable {
-        mutableStateOf(1)
+        mutableStateOf(0)
     }
     BottomNavigation {
         destinationList.forEachIndexed { index, destination ->
@@ -50,7 +71,7 @@ fun MyBottomNavigation(navController: NavController) {
                         popUpTo(Home.route)
                     }
                 }
-            ) { }
+            )
         }
     }
 }

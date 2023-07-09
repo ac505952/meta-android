2a10dcb9f946dbc6999a275a9fca487c36d68e56
Belajar <bljkmp@gmail.com>
2023-07-09 23:56:02 +0700

    Add a private responseLiveData field. Set it to a MutableLiveData instance with holding String data

--- c7m2l1-exercise-http-RequestResponse-in-kotlin/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ c7m2l1-exercise-http-RequestResponse-in-kotlin/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.MutableLiveData
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 
 class MainActivity : ComponentActivity() {
     private val httpClient= HttpClient(Android)
+    private val responseLiveData= MutableLiveData<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {

c3297c0dd19c47ca40b07ee9263444d8072c6f33
Belajar <bljkmp@gmail.com>
2023-07-12 11:29:37 +0700

    Declare the database structure

--- C7M4L2-Exercise-Read-and-write-with-Room/app/src/main/java/com/littlelemon/littlelemonmenueditor/MenuItem.kt
+++ C7M4L2-Exercise-Read-and-write-with-Room/app/src/main/java/com/littlelemon/littlelemonmenueditor/MenuItem.kt
@@ -1,7 +1,33 @@
 package com.littlelemon.littlelemonmenueditor
 
+import androidx.lifecycle.LiveData
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+
+@Entity
 data class MenuItem(
-    val id: String,
+    @PrimaryKey val id: String,
     val name: String,
     val price: Double
 )
+
+interface MenuDao {
+    @Query("SELECT * FROM MenuItem")
+    fun getAllMenuItems(): LiveData<List<MenuItem>>
+
+    @Insert
+    fun saveMenuItem(menuItem: MenuItem)
+
+    @Delete
+    fun deleteMenuItem(menuItem: MenuItem)
+}
+
+@Database(entities = [MenuItem::class], version = 1)
+abstract class MenuDatabase: RoomDatabase() {
+    abstract fun menuDao(): MenuDao
+}

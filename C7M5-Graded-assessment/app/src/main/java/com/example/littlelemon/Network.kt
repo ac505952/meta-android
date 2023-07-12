a1ddd1e4047bf7a5a14ff6abc02dc393b6caa962
Belajar <bljkmp@gmail.com>
2023-07-12 22:38:27 +0700

    Representing the menu and menu items

--- C7M5-Graded-assessment/app/src/main/java/com/example/littlelemon/Network.kt
+++ C7M5-Graded-assessment/app/src/main/java/com/example/littlelemon/Network.kt
@@ -5,12 +5,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MenuNetwork(
-    // add code here
+    val id: Int,
+    val title: String,
+    val price: String
 )
 
 @Serializable
 data class MenuItemNetwork(
-    // add code here
+    val menu: List<MenuNetwork>
 ) {
     fun toMenuItemRoom() = MenuItemRoom(
         // add code here
